ecdea740b47302e18e9c00641eafda93e2bc0adc
wangyu <1837960253@qq.com>
2018-01-22 10:05:02 +0800

    增加glide配置

---
+++ app/src/main/java/com/swuos/mobile/app/SWUAppGlideModule.java
@@ -0,0 +1,11 @@
+package com.swuos.mobile.app;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * GlideApp注册
+ * Created by wangyu on 2017/12/19.
+ */
+@GlideModule
+public final class SWUAppGlideModule extends AppGlideModule {}
